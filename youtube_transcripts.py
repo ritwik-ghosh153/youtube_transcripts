994ac337832c4b22a855df7ea9a9a6cd96002599
ritwik_ghosh153 <ritwik.ghosh1010@gmail.com>
2021-05-28 19:54:54 +0530

    added readme.txt

--- youtube_transcripts.py
+++ youtube_transcripts.py
@@ -8,22 +8,10 @@ import sys
 application_path=""
 
 if getattr(sys, 'frozen', False):
-    # If the application is run as a bundle, the PyInstaller bootloader
-    # extends the sys module by a flag frozen=True and sets the app 
-    # path into variable _MEIPASS'.
     application_path = os.path.dirname(sys.executable)
 else:
     application_path = os.path.dirname(os.path.abspath(__file__))
 
-
-# def resource_path(relative):
-#     return os.path.join(
-#         os.environ.get(
-#             "_MEIPASS2",
-#             os.path.abspath(".")
-#         ),
-#         relative
-#     )
 
 def download_subs(url, lang, auto=False):
     print(lang)
@@ -34,7 +22,6 @@ def download_subs(url, lang, auto=False):
         "writesubtitles": True,
         "subtitlesformat": "vtt",
         "subtitleslangs": [lang],
-        # "listsubtitles": True
     }
 
     with youtube_dl.YoutubeDL(opts) as yt:
@@ -48,7 +35,6 @@ with open(os.path.join(application_path, 'links.csv')) as f:
 links=links[1:]
 
 for url in links:
-    # url = "https://www.youtube.com/watch?v=32laLeJozDM"
     download_subs(url, lang="en", auto=True)
     download_subs(url, lang="zh-Hans", auto=False)
     download_subs(url, lang="ko", auto=False)
@@ -73,15 +59,8 @@ for file in os.listdir(os.path.join(application_path, 'subs/')):
             transcript += " " + line
             previous = line
 
-        # print(transcript)
         txt_name =  re.sub(r'.vtt$', '.txt', file)
         if not os.path.exists(os.path.join(application_path, 'transcripts/')):
             os.makedirs(os.path.join(application_path, 'transcripts/'))
         with open(os.path.join(application_path, f'transcripts/{txt_name}'),'w') as f:
             f.write(transcript)
-
-        # for caption in webvtt.read('Negotiating M&A deal terms-32laLeJozDM.en.vtt'):
-        #     print(caption.start)
-        #     print(caption.end)
-        #     print(caption.text)
-        #     webvtt.from_sbv('captions.sbv').save()
